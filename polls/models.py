8bf34995f768609df9bcabd864efeb4fc6969704
alouifahima <47674122+alouifahima@users.noreply.github.com>
2020-09-02 22:22:30 +0100

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -2,26 +2,21 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Machine_virtuelle(models.Model):
+    user   = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    type_OS = models.CharField(max_length=200)
+    version_OS = models.CharField(max_length=200)
+    hard_disk_size = models.FloatField(default=0)
+    ram = models.IntegerField(default=0)
     def __str__(self):
-        return self.question_text
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
-    was_published_recently.admin_order_field = 'pub_date'
-    was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
+        return self.name
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+class Abonnement(models.Model):
+    user   = models.ForeignKey(User, on_delete=models.CASCADE)
+    type_Abonnement = models.CharField(max_length=200)
+    date_joind = models.DateTimeField(auto_now=True)
     def __str__(self):
-        return self.choice_text
+        return self.user
